45b671c555644eae579adb4db011b9f1177a58c3
Tryfon <tryfongav@gmail.com>
2026-02-23 03:47:39 +0200

    Fix: trust proxy for Render reverse proxy

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const User = require('./models/User');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.set('trust proxy', 1);
 
 app.use(helmet({ contentSecurityPolicy: false }));
 app.use(express.json({ limit: '1mb' }));
